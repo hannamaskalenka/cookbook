5729883154910923d53167c4707132284d160ee1
Aksiuszka <70382984+Aksiuszka@users.noreply.github.com>
2022-11-18 14:28:24 +0100

    feature/create-react-app, eslint and prettier configuration (#2)
    * feature/create-react-app, eslint and prettier configuration
    
    * dev dependencies fix
    
    * moved @types to dev dependencies

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
+  settings: {
+    'import/parsers:': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        moduleDirectory: ['node_modules', 'src'],
+      },
+    },
+  },
+  ignorePatterns: ['**/*.js', '**/*.json', 'node_modules', 'public', 'coverage', 'dist', 'built'],
+  rules: {
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+      },
+    ],
+    'react-destructuring-assignment': 'off',
+    'react-props-no-spreading': 'off',
+    'arrow-body-style': 'off',
+    'prettier/prettier': 'error',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/*.test.ts', '**/*.test.tsx', 'src/tests/**/*', 'src/setupTests.ts'],
+      },
+    ],
+  },
+};
