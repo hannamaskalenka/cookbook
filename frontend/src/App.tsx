99fec9d19ee5dd84d80c18f6b0aa49e1b2dfdc8e
Anna Kawecka <aakawecka89@gmail.com>
2022-12-06 12:37:43 +0100

    feature/ added react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
-import Test from 'components/Test/Test';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
 
 const App = () => {
   return (
-    <>
-      <Test />
-      <p>Its good to see you!</p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 };
 
