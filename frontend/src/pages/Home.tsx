71e6e0ab797f0d05324c04086fe7b55fdd59737e
Anna Kawecka <aakawecka89@gmail.com>
2022-12-08 12:03:37 +0100

    feature/mui-theme updated

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,16 @@
+import { Button, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
-      <button type="button">
-        <Link to="/dashboard">Check dashboard by clicking</Link>
-      </button>
+      <Button variant="contained" sx={{ bgcolor: 'accent.dark' }} type="button">
+        test
+      </Button>
+      <Link to="/dashboard">Check dashboard by clicking</Link>
+      <Typography sx={{ fontFamily: (theme) => theme.typography.display }}>
+        Test Typography 0001
+      </Typography>
     </div>
   );
 };
