7a04d948a6c3dabd8bb6d06f1cb855d7011cd926
Hanna Maskalenka <hanna.maskalenka@itechart-group.com>
2022-12-14 17:02:58 +0100

    add react-query & examples

---
+++ frontend/src/pages/HomePage/Home.tsx
@@ -0,0 +1,80 @@
+import { CircularProgress, TextField, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import Button from 'shared/components/Button/Button';
+import { useGetRecipes, useLoginUser } from './hooks';
+
+const Home = () => {
+  const [token, setToken] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { mutate: login, isSuccess } = useLoginUser(setToken);
+
+  const { refetch, data: recipes, isFetching } = useGetRecipes(token);
+
+  useEffect(() => {
+    if (isSuccess) {
+      refetch();
+    }
+  }, [refetch, isSuccess]);
+
+  const handleRequestButtonClick = async () => {
+    login({ username, password });
+  };
+
+  return (
+    <div>
+      <div>
+        <Button label="test" type="secondary" />
+        <Button
+          type="primary"
+          onClick={handleRequestButtonClick}
+          label={<Typography variant="regular">Send Login Request</Typography>}
+        />
+      </div>
+      <div>
+        <TextField
+          variant="filled"
+          name="username"
+          placeholder="Username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <TextField
+          variant="filled"
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
+
+      <Link to="/dashboard">Check dashboard by clicking</Link>
+      <Typography variant="display">Test Typography 0001</Typography>
+
+      {isFetching ? (
+        <CircularProgress />
+      ) : (
+        <div>
+          Recipes:
+          {recipes?.length ? (
+            recipes.map((recipe: any) => {
+              return (
+                <div key={recipe?.id}>
+                  <Typography variant="regular">{recipe?.title}</Typography>
+                </div>
+              );
+            })
+          ) : (
+            <Typography>No recipes found</Typography>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
