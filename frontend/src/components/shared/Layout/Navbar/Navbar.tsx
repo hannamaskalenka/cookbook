1403cb82b8ac1f4e136653598ff0cb2c2a4f289b
Anna Kawecka <aakawecka89@gmail.com>
2022-12-15 15:49:10 +0100

    comment removed

--- frontend/src/components/shared/Layout/Navbar/Navbar.tsx
+++ frontend/src/components/shared/Layout/Navbar/Navbar.tsx
@@ -122,40 +122,6 @@ function Navbar() {
               <Dropdown closeDropdown={() => setShowDropdown(false)} />
             )}
           </DropdownWrapper>
-          {/* <p>dropdown</p>
-          <Box onClick={navhelper} sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem
-                  sx={{ width: '15rem', bgcolor: 'white' }}
-                  key={setting}
-                  onClick={handleCloseUserMenu}
-                >
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box> */}
         </Toolbar>
       </Container>
     </Navigation>
