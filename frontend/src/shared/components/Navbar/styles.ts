8ef0b96824a16eeac3e25edc502bfb8442c5b5ce
Anna Kawecka <aakawecka89@gmail.com>
2023-01-19 13:51:06 +0100

    feature/mui button add variants

--- frontend/src/shared/components/Navbar/styles.ts
+++ frontend/src/shared/components/Navbar/styles.ts
@@ -121,6 +121,9 @@ export const useStyles = makeStyles((theme: Theme) =>
       width: '100%',
       alignItems: 'center',
     },
+    loginButton: {
+      padding: theme.spacing(0.5, 2.3),
+    },
   }),
 );
 
